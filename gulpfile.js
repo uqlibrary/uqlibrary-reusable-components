e665ebda0ab4219ae23d17379c24d5d4e20f8a87
Cameron Green <cam@uq.edu.au>
2015-09-01 16:11:08 +1000

    Removing application header, trimming npm down

--- gulpfile.js
+++ gulpfile.js
@@ -14,9 +14,9 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var del = require('del');
 var runSequence = require('run-sequence');
-var browserSync = require('browser-sync');
+//var browserSync = require('browser-sync');
 var vulcanize = require('gulp-vulcanize');
-var reload = browserSync.reload;
+//var reload = browserSync.reload;
 var merge = require('merge-stream');
 var path = require('path');
 var fs = require('fs');
@@ -53,11 +53,11 @@ gulp.task('jshint', function () {
     config.elements + '/**/*.js',
     config.elements + '/**/*.html'
   ])
-    .pipe(reload({stream: true, once: true}))
+    //.pipe(reload({stream: true, once: true}))
     .pipe($.jshint.extract()) // Extract JS from .html files
     .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
+    .pipe($.jshint.reporter('jshint-stylish'));
+    //.pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
 });
 
 // Vulcanize imports
