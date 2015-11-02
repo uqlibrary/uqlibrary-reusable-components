8b6d6f5bae3169dafbcfea41e403c6cd08c6bd33
Cameron Green <cam@uq.edu.au>
2015-11-02 15:50:19 +1000

    Adding test artefacts

---
+++ wct.conf.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+var ret = {
+  'suites': ['test'],
+  'webserver': {
+    'pathMappings': []
+  },
+  plugins: {
+    local: {
+     browsers: [
+        'firefox'
+      ]
+    }
+  }
+};
+
+var mapping = {};
+var rootPath = (__dirname).split(path.sep).slice(-1)[0];
+
+mapping['/components/' + rootPath +
+'/bower_components'] = 'bower_components';
+
+ret.webserver.pathMappings.push(mapping);
+
+module.exports = ret;
