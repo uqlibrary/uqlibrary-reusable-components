52fd6b83042786b100e7123c4e8124fd7b429e15
jannakha <j_malikova@yahoo.com>
2015-11-18 12:02:45 +1000

    added ia button

---
+++ elements/uql-ia-button/uql-ia-button.js
@@ -0,0 +1,41 @@
+(function () {
+  Polymer({
+    is: 'uql-ia-button',
+    properties: {
+      showTitle: {
+        type: Boolean,
+        value: true
+      },
+      isIAEnabled: {
+        type: Boolean,
+        value: false,
+        observer: '_IAEnabledChanged'
+      },
+      buttonTitle: {
+        type: String,
+        value: "Enable IA"
+      }
+    },
+
+    ready: function() {
+      var that = this;
+      this.isIAEnabled = document.cookie.indexOf("toggleia=1") >= 0;
+    },
+
+    _IAEnabledChanged: function(newValue, oldValue) {
+      this.buttonTitle = newValue ? "Disable IA" : "Enable IA";
+    },
+
+    toggleIA: function() {
+      if (this.isIAEnabled){
+        document.cookie = "toggleia=0";
+      } else {
+        document.cookie = "toggleia=1";
+      }
+
+      window.location.reload(true);
+    }
+
+  });
+})();
+
