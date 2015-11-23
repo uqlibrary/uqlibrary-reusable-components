f26fea71bc502645358eedbb9fedf05165143336
Zeh Ferrari <e@zeh.me>
2015-11-23 17:15:41 +1000

    #106704562 - new connect footer [in progress...]

---
+++ elements/uql-connect-footer/uql-connect-footer.js
@@ -0,0 +1,21 @@
+(function() {
+  Polymer({
+    is: 'uql-connect-footer',
+    cfmenu: {},
+    jsonURL: '../uql-connect-footer/uql-connect-footer.json',
+    properties: {
+      verbose: {
+        type: Boolean,
+        value: true
+      }
+    },
+
+    _handleError: function (event) {
+      console.log(event);
+    },
+    _handleResponse: function (event) {
+      this.cfmenu = event.detail.response;
+      console.log(this.cfmenu);
+    }
+  });
+})();
