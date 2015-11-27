f32e22659fa079858f14ebef807ce828f96c2f70
jannakha <j_malikova@yahoo.com>
2015-11-27 16:20:05 +1000

    fixing styles

--- applications/libwww/load.js
+++ applications/libwww/load.js
@@ -7,12 +7,12 @@ window.addEventListener('WebComponentsReady', function() {
   // insert header after body-tag
   var header = document.createElement('uq-minimal-header');
   header.showIAButton = true;
+  header.showMenuButton = true;
   document.body.insertBefore(header, firstElement);
 
   // insert menu after header
   var menu = document.createElement('uql-menu');
   menu.menuJson = menuJson;
-  //document.body.insertBefore(menu, firstElement);
   header.appendChild(menu);
 
   header.addEventListener("menu-clicked", function(event) {
@@ -20,6 +20,10 @@ window.addEventListener('WebComponentsReady', function() {
   });
 
   // insert footer before body-tag
+  var subFooter = document.createElement('uql-connect-footer');
+  document.body.appendChild(subFooter);
+  subFooter.footerMenu = menuJson;
+
   var footer = document.createElement('uq-minimal-footer');
   document.body.appendChild(footer);
 });
