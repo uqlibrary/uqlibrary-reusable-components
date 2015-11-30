0a4ab82ed482f3c7ea977f6914ad7a9c0a3748ad
jannakha <j_malikova@yahoo.com>
2015-11-30 14:29:31 +1000

    testing sub footer

--- applications/libanswers/load.js
+++ applications/libanswers/load.js
@@ -5,6 +5,11 @@ window.addEventListener('WebComponentsReady', function(e) {
   document.body.insertBefore(header, document.body.children[0]);
 
   // insert footer before body-tag
+  var subFooter = document.createElement('uql-connect-footer');
+  document.body.appendChild(subFooter);
+  subFooter.footerMenu = '//assets.library.uq.edu.au/reusable-components/resources/uql-menu.json';
+
+  // insert footer before body-tag
   var footer = document.createElement('uq-minimal-footer');
   document.body.appendChild(footer, document.body);
 
