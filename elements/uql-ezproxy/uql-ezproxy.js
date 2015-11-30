363147878c7f3e309f2d3685a89871a81278ee72
Zeh Ferrari <e@zeh.me>
2015-11-30 12:15:54 +1000

    DOI and create a link mode implemented.

--- elements/uql-ezproxy/uql-ezproxy.js
+++ elements/uql-ezproxy/uql-ezproxy.js
@@ -1,33 +1,110 @@
 Polymer({
   is: 'uql-ezproxy',
+  target:'',
+  properties: {
+    createLink: {
+      type: Boolean,
+      value: false
+    },
+    label: {
+      type: String,
+      value: ''
+    },
+    hide: {
+      type: Boolean,
+      value: true
+    },
+    url: {
+      type: String,
+      value: ''
+    },
+    result: {
+      type: String,
+      value: ''
+    }
+  },
   _submit: function () {
-    var valid = this.checkURL();
-    this.$.urlContainer.invalid = !valid;
-    if (valid) {
-      var url = 'http://ezproxy.library.uq.edu.au/login?url=' + this.$.url.value;
-      var win = window.open(url, '_blank');
+    if (this.createLink) {
+      this.showUrl();
+    } else {
+      this.goProxie();
+    }
+  },
+  showUrl: function () {
+    if (this.checkURL()) {
+      this.panelToggle();
+      this.hide = false;
+    }
+  },
+  goProxie: function () {
+    if (this.checkURL()) {
+      var win = window.open(this.target, '_blank');
       win.focus();
     }
   },
-
   checkURL: function () {
+    var valid = false;
     var dest = this.$.url.value;
+    var doi = /\b(10[.][0-9]{3,}(?:[.][0-9]+)*\/(?:(?!["&\'])\S)+)\b/;
+
     if (dest.length <= 0) {
       this.$.errorMsg.textContent = "Please enter a URL";
-      return false;
+    } else if(doi.test(dest)) {
+      valid = true;
     } else if (!validator.isURL(dest, {require_protocol: true})) {
       if (dest.substring(0, 4).toLowerCase() !== 'http') {
         this.$.errorMsg.textContent = "Invalid URL. Please add the protocol ie: http://, https://";
       } else {
         this.$.errorMsg.textContent = "Invalid URL.";
       }
-      return false;
     } else if (dest.indexOf('ezproxy.library.uq.edu.au') >= 0) {
       this.$.errorMsg.textContent = "The domain ezproxy.library.uq.edu.au cannot be used in your URL";
-      return false;
     } else {
       this.$.errorMsg.textContent = "";
+      valid = true;
+    }
+
+    this.$.urlContainer.invalid = !valid;
+    if (valid) {
+      this.target = 'http://ezproxy.library.uq.edu.au/login?url=';
+      if (doi.test(dest)) {
+        this.target += 'http://dx.doi.org/';
+      }
+      this.target += dest;
       return true;
+    }
+  },
+  panelToggle: function() {
+    this.hide=!this.hide;
+    if(this.hide) {
+      this.$.url.value = "";
+      this.$.urlContainer.invalid = false;
+      this.$.url.focus();
+    }
+  },
+  grabUrl: function() {
+    this.$.target.select();
+
+    try {
+      var successful = document.execCommand('copy');
+      this.msg = 'Copying text command was ' + (successful ? 'copied' : 'error');
+    } catch (err) {
+      this.msg = 'Oops, unable to copy.';
+    }
+    this.$.notification.show();
+  },
+/*
+  The ready callback is called when an element’s local DOM is ready.
+  It is called after the element’s template has been stamped and all elements
+  inside the element’s local DOM have been configured (with values bound from
+  parents, deserialized attributes, or else default values) and had their
+  ready method called.
+*/
+  ready: function() {
+    if(this.createLink) {
+      this.label='Create Link';
+    } else {
+      this.label='Go';
     }
   }
 });
