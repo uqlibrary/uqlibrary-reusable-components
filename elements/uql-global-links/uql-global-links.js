3e4b8ed26d741a3407d354ef3f77e439b293e444
Zeh Ferrari <e@zeh.me>
2016-01-08 09:36:02 +1000

    global links documentation [in progress]

--- elements/uql-global-links/uql-global-links.js
+++ elements/uql-global-links/uql-global-links.js
@@ -14,12 +14,17 @@
       },
 
       /**
-       * array containing links as objects where:
-       *
-       *  url: target web link (http|https)://domain/path
-       *  title: text/label
-       *  accessKey: browser keyboard shortcut
-       *  showSeparator: enable/disable the separator string after the link
+       * array containing links as objects.
+       * [
+       *   { url: "http://www.uq.edu.au/", title: "UQ Home", accessKey: 1, showSeparator: true },
+       *   { url: "http://www.uq.edu.au/contacts/", title: "Contacts", accessKey: 2, showSeparator: true },
+       *   { url: "http://www.uq.edu.au/study/", title: "Study", accessKey: 3, showSeparator: true },
+       *   { url: "http://www.uq.edu.au/maps/", title: "Maps", accessKey: 4, showSeparator: true },
+       *   { url: "http://www.uq.edu.au/news/", title: "News", accessKey: 5, showSeparator: true },
+       *   { url: "http://www.uq.edu.au/events/", title: "Events", accessKey: 6, showSeparator: true },
+       *   { url: "http://jobs.uq.edu.au/", title: "Jobs", accessKey: 7, showSeparator: true },
+       *   { url: "http://my.uq.edu.au/", title: "my.UQ", accessKey: 9, showSeparator: false }
+       * ];
        *
        * @type {{elements: Array<HTMLElement>, level: object}}
        */
