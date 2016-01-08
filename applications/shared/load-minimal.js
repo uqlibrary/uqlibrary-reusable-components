cb3b68af41e9c94215c3dd0ca00d653a4941d0a4
jannakha <j_malikova@yahoo.com>
2016-01-08 18:09:12 +1000

    optimized loading for all apps

--- applications/shared/load-minimal.js
+++ applications/shared/load-minimal.js
@@ -1,13 +1,27 @@
-window.addEventListener('WebComponentsReady', function(e) {
+//insert elements, even before Polymer is loaded
 
-  // insert header after body-tag
-  var header = document.createElement('uq-minimal-header');
+//first element of the original document
+var firstElement = document.body.children[0];
+
+// insert header after body-tag
+var header = document.createElement('uq-minimal-header');
+document.body.insertBefore(header, firstElement);
+
+// insert footer before body-tag
+var footer = document.createElement('uq-minimal-footer');
+document.body.appendChild(footer);
+// or this?
+//document.body.appendChild(footer, document.body);
+
+window.addEventListener('WebComponentsReady', function() {
+  // when polymer is ready - configure elements
+
   header.showLoginButton = false;
 
-  document.body.insertBefore(header, document.body.children[0]);
-
-  // insert footer before body-tag
-  var footer = document.createElement('uq-minimal-footer');
-  document.body.appendChild(footer, document.body);
-
+  //reveal elements with easing in effect
+  var content = document.querySelectorAll(".loading");
+  for(var i=0; i< content.length; i++){
+    var element = content[i];
+    element.removeAttribute('unresolved');
+  }
 });
--- applications/uqlapp/load.js
+++ applications/uqlapp/load.js
@@ -1,17 +1,30 @@
-window.addEventListener('WebComponentsReady', function(e) {
+//insert elements, even before Polymer is loaded
 
-  // insert header after body-tag
-  var header = document.createElement('uq-minimal-header');
-  document.body.insertBefore(header, document.body.children[0]);
+//first element of the original document
+var firstElement = document.body.children[0];
 
-  // insert footer before body-tag
-  var subFooter = document.createElement('uql-connect-footer');
-  document.body.appendChild(subFooter);
-  subFooter.footerMenuUrl = '//assets.library.uq.edu.au/reusable-components/resources/uql-menu.json';
+// insert header after body-tag
+var header = document.createElement('uq-minimal-header');
+document.body.insertBefore(header, firstElement);
+
+// insert sub footer before body-tag
+var subFooter = document.createElement('uql-connect-footer');
+document.body.appendChild(subFooter);
+
+// insert footer before body-tag
+var footer = document.createElement('uq-minimal-footer');
+document.body.appendChild(footer);
+
+window.addEventListener('WebComponentsReady', function() {
+  // when polymer is ready - configure elements
+
+  //set up domain for links in sub footer
   subFooter.mainDomain = 'https://www.library.uq.edu.au';
 
-  // insert footer before body-tag
-  var footer = document.createElement('uq-minimal-footer');
-  document.body.appendChild(footer, document.body);
-
+  //reveal elements with easing in effect
+  var content = document.querySelectorAll(".loading");
+  for(var i=0; i< content.length; i++){
+    var element = content[i];
+    element.removeAttribute('unresolved');
+  }
 });
