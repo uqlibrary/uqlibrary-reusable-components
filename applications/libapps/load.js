63566e2f6ca05c77865ec859223b2a290b355fcf
jannakha <j_malikova@yahoo.com>
2016-01-08 18:36:32 +1000

    optimized loading for libapps

--- applications/libapps/load.js
+++ applications/libapps/load.js
@@ -1,19 +1,31 @@
-//insert elements, even before Polymer is loaded
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
 
-//first element of the original document
-var firstElement = document.body.children[0];
+function loadReusableComponents() {
+  //insert elements, even before Polymer is loaded
 
-// insert header after body-tag
-var header = document.createElement('uq-minimal-header');
-document.body.insertBefore(header, firstElement);
+  //first element of the original document
+  var firstElement = document.body.children[0];
 
-// insert sub footer before body-tag
-var subFooter = document.createElement('uql-connect-footer');
-document.body.appendChild(subFooter);
+  // insert header after body-tag
+  var header = document.createElement('uq-minimal-header');
+  document.body.insertBefore(header, firstElement);
 
-// insert footer before body-tag
-var footer = document.createElement('uq-minimal-footer');
-document.body.appendChild(footer);
+  // insert sub footer before body-tag
+  var subFooter = document.createElement('uql-connect-footer');
+  document.body.appendChild(subFooter);
+
+  // insert footer before body-tag
+  var footer = document.createElement('uq-minimal-footer');
+  document.body.appendChild(footer);
+}
+
+ready(loadReusableComponents);
 
 window.addEventListener('WebComponentsReady', function() {
   // when polymer is ready - configure elements
