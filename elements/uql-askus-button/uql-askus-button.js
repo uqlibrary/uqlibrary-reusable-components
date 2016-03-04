7c46b77b1b8d7cdd58d0117b87190aeaa077d782
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-03-04 10:52:21 +1000

    Removed chat button and replaced with new Ask Us button. Fixed issue in the alerts API

---
+++ elements/uql-askus-button/uql-askus-button.js
@@ -0,0 +1,51 @@
+(function () {
+  Polymer({
+    is: 'uql-askus-button',
+    properties: {
+
+    },
+    attached: function () {
+      this.$.callout.calloutItems = {
+        "items": [
+          {
+            "label": "FAQ",
+            "icon": "communication:import-contacts",
+            "link": "https://http://answers.library.uq.edu.au/"
+          },
+          {
+            "label": "In Person",
+            "icon": "social:people",
+            "link": "https://www.library.uq.edu.au/locations-hours/service-points"
+          },
+          {
+            "label": "Chat",
+            "icon": "communication:chat",
+            "link": "https://v2.libanswers.com/chati.php?iid=1193&hash=fdbdf3c1190c1b6147b92d38c20194a8&online=true&referer=https%3A%2F%2Fwww.library.uq.edu.au%2F",
+            "disabled": "chat-offline"
+          },
+          {
+            "label": "Phone",
+            "icon": "communication:call",
+            "link": "https://www.library.uq.edu.au/contact-us",
+            "linkMobile": "tel: +61733464312",
+            "disabled": "chat-offline"
+          },
+          {
+            "label": "Email",
+            "icon": "communication:email",
+            "link": "mailto:askus@library.uq.edu.au"
+          },
+          {
+            "label": "Form",
+            "icon": "hardware:desktop-mac",
+            "link": "http://answers.library.uq.edu.au/ask"
+          }
+        ],
+        "summary": {
+          "label": "More ways to contact us",
+          "link": "https://www.library.uq.edu.au/contact-us"
+        }
+      };
+    }
+  });
+})();
---
+++ elements/uql-callout-button/uql-callout-button.js
@@ -0,0 +1,151 @@
+(function () {
+  Polymer({
+    is: 'uql-callout-button',
+    properties: {
+      /**
+       * Sets the width of the callout window
+       */
+      calloutWidth: {
+        type: Number,
+        value: 280
+      },
+      /**
+       * The menu for the callout. See http://github.com/uqlibrary/uqlibrary-callout for more details
+       */
+      calloutItems: {
+        type: Object,
+        observer: "_calloutItemsChanged"
+      },
+      /**
+       * Icon to be used in the paper-button
+       */
+      icon: {
+        type: String,
+        value: "social:people"
+      },
+      /**
+       * Label to place on the paper-button
+       */
+      buttonLabel: {
+        type: String,
+        value: "My button"
+      }
+    },
+    ready: function () {
+
+    },
+    _calloutItemsChanged: function () {
+      this.$.callout.menu = this.calloutItems;
+    },
+    /**
+     * Opens the Ask Us callout. Calculates whether to move the whole callout and/or the arrow
+     * @private
+     */
+    _openCallout: function () {
+      this._alignCallout();
+
+      this.$.dropdown.open();
+    },
+    /**
+     * Aligns the callout appropriate to the screen
+     * @private
+     */
+    _alignCallout: function () {
+      var screenWidth = window.innerWidth || document.getElementsByTagName('body')[0].clientWidth;
+      var buttonBounds = this.$.button.getBoundingClientRect();
+
+      if (this._canFitRight(screenWidth, buttonBounds)) {
+        // Right align the callout
+        this._alignRight();
+      } else if (this._canFitCenter(screenWidth, buttonBounds)) {
+        // Center align the callout
+        this._alignCenter(buttonBounds);
+      } else if (this._canFitLeft(screenWidth, buttonBounds)) {
+        this._alignLeft(buttonBounds);
+      } else {
+        this._alignCenterScreen(buttonBounds, screenWidth);
+      }
+    },
+    /**
+     * Aligns the callout to the left of the button
+     * @param buttonBounds
+     * @private
+     */
+    _alignLeft: function (buttonBounds) {
+      this.$.dropdown.horizontalOffset = 0 - this.calloutWidth + (buttonBounds.width);
+
+      this.$.callout.arrowHorizontalAlign = "left";
+      this.$.callout.arrow = true;
+    },
+    /**
+     * Aligns the callout on the right of the button
+     * @private
+     */
+    _alignRight: function () {
+      this.$.dropdown.horizontalOffset = 0;
+
+      this.$.callout.arrowHorizontalAlign = "right";
+      this.$.callout.arrow = true;
+    },
+    /**
+     * Aligns the callout on the center of the button
+     * @param buttonBounds
+     * @private
+     */
+    _alignCenter: function (buttonBounds) {
+      this.$.dropdown.horizontalOffset = 0 - (this.calloutWidth - buttonBounds.width) / 2;
+      this.$.callout.arrowHorizontalAlign = "center";
+      this.$.callout.arrow = true;
+    },
+    /**
+     * Aligns the callout on the center of the screen
+     * @param buttonBounds
+     * @param screenWidth
+     * @private
+     */
+    _alignCenterScreen: function (buttonBounds, screenWidth) {
+      var desiredX = (screenWidth - (screenWidth - this.calloutWidth) / 2);
+      if (desiredX < 0) { desiredX = screenWidth; }
+
+      this.$.dropdown.horizontalOffset = (desiredX - buttonBounds.right) * -1;
+      this.$.callout.arrow = false;
+
+      console.log("HMM?", this.$.callout.arrow);
+    },
+    /**
+     * Checks if the callout can fit when left aligned on the button
+     * @param screenWidth
+     * @param buttonBounds
+     * @returns {boolean}
+     * @private
+     */
+    _canFitLeft: function (screenWidth, buttonBounds) {
+      return (screenWidth - buttonBounds.left >= this.calloutWidth);
+    },
+    /**
+     * Checks if the callout can fit when right aligned on the BUTTON
+     * @param screenWidth
+     * @param buttonBounds
+     * @private
+     */
+    _canFitRight: function (screenWidth, buttonBounds) {
+      return (buttonBounds.right >= this.calloutWidth);
+    },
+    /**
+     * Checks if the callout will fit when center aligned on the BUTTON
+     * @param screenWidth
+     * @param buttonBounds
+     * @returns {boolean}
+     * @private
+     */
+    _canFitCenter: function (screenWidth, buttonBounds) {
+      var pokingOut = (this.calloutWidth - buttonBounds.width) / 2;
+
+      if (buttonBounds.left >= pokingOut && (screenWidth - buttonBounds.right) >= pokingOut) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  });
+})();
