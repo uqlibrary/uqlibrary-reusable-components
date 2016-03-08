bb7306361e81c747e22d58cafd797ac02ceb69e5
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-03-08 15:41:23 +1000

    Removed my applications buttons and replaced with older version. Added GA events to login / logout

--- elements/uql-apps-button/uql-apps-button.js
+++ elements/uql-apps-button/uql-apps-button.js
@@ -1,69 +1,28 @@
+
 (function () {
   Polymer({
     is: 'uql-apps-button',
     properties: {
-      /**
-       * Holds the account for the Applications API check
-       */
-      account: {
-        type: Object,
-        value: {
-          hasSession: false
-        }
+      /** Whether to display title of the button */
+      showTitle: {
+        type: Boolean,
+        value: true
       },
-      /**
-       * Base URL for the My Library Applications
-       */
-      _applicationUrl: {
+      /** Button title text */
+      buttonTitle: {
         type: String,
-        value: ""
+        value: "My Library"
+      },
+      /** Value of redirect URL */
+      redirectUrl: {
+        type: String,
+        value: "https://www.library.uq.edu.au/mylibrary"
       }
     },
-    attached: function () {
-      var self = this;
 
-      // Event listener for Account
-      this.$.apiAccount.addEventListener('uqlibrary-api-account-loaded', function (e) {
-        if (e.detail.hasSession) {
-          self.account = e.detail;
-          self.$.apiApplications.get();
-        }
-      });
-      this.$.apiAccount.get();
-
-      // Event listener for Applications
-      this.$.apiApplications.addEventListener('uqlibrary-api-applications-loaded', function (e) {
-        self._generateCalloutMenu(e.detail);
-      });
-
-      this.$.apiApplications.get();
-    },
-    /**
-     * Parses the API response from uqlibrary-api-applications into an object uqlibrary-callout reads
-     * @param data
-     * @private
-     */
-    _generateCalloutMenu: function (data) {
-      var menu = {
-        items: [],
-        summary: {
-          label: "Logout",
-          link: "https://www.library.uq.edu.au/logout"
-        }
-      };
-
-      // Generate menu items
-      data.forEach(function (item) {
-        if (item.isDivider || item.link === '/home' || item.link === "https://www.library.uq.edu.au/") { return; }
-
-        menu.items.push({
-          label: item.title,
-          link: 'https://app.library.uq.edu.au/v1' + item.link,
-          icon: item.icon
-        });
-      });
-
-      this.$.callout.calloutItems = menu;
+    /** Redirects to the specified URL */
+    _myLibraryClicked: function() {
+      window.location.href = this.redirectUrl;
     }
   });
 })();
