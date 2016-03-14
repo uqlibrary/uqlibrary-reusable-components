c139ff1e5347598b38b9882a26ef3983ed78d7ee
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-03-14 15:50:17 +1000

    Implemented changes as per the PT ticket

--- elements/uql-askus-button/uql-askus-button.js
+++ elements/uql-askus-button/uql-askus-button.js
@@ -18,17 +18,17 @@
             "link": "http://answers.library.uq.edu.au/"
           },
           {
-            "label": "In Person",
-            "icon": "social:people",
-            "link": "https://www.library.uq.edu.au/locations-hours/service-points"
-          },
-          {
             "label": "Chat",
             "icon": "communication:chat",
             "link": "https://v2.libanswers.com/chati.php?iid=1193&hash=fdbdf3c1190c1b6147b92d38c20194a8&online=true&referer=https%3A%2F%2Fwww.library.uq.edu.au%2F",
             "disabled": "chat-offline",
             "target": "chatWindow",
             "targetOptions": "width=400, height=400"
+          },
+          {
+            "label": "Email",
+            "icon": "communication:email",
+            "link": "mailto:askus@library.uq.edu.au"
           },
           {
             "label": "Phone",
@@ -38,14 +38,14 @@
             "disabled": "chat-offline"
           },
           {
-            "label": "Email",
-            "icon": "communication:email",
-            "link": "mailto:askus@library.uq.edu.au"
+            "label": "Contact Form",
+            "icon": "device:dvr",
+            "link": "http://answers.library.uq.edu.au/ask"
           },
           {
-            "label": "Form",
-            "icon": "hardware:desktop-mac",
-            "link": "http://answers.library.uq.edu.au/ask"
+            "label": "Come see us",
+            "icon": "social:people",
+            "link": "https://www.library.uq.edu.au/locations-hours/service-points"
           }
         ],
         "summary": {
