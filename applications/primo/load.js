60a348d150765b5300c00a54a83e6f46e5c0612a
jannakha <j_malikova@yahoo.com>
2016-06-08 16:18:10 +1000

    removed my library button from header for primo

--- applications/primo/load.js
+++ applications/primo/load.js
@@ -38,6 +38,7 @@ function loadReusableComponents() {
   window.addEventListener('WebComponentsReady', function () {
     // when polymer is ready - configure elements
     header.showLoginButton = false;
+    header.showAppsButton = false;
   });
 }
 
