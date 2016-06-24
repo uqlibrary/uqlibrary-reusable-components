c073b465da8fe323a0941e344a3eaa3d0d5470a5
jannakha <j_malikova@yahoo.com>
2016-06-24 16:07:02 +1000

    fixing login bar width

--- applications/primo/load.js
+++ applications/primo/load.js
@@ -109,7 +109,7 @@ function loadSigninNotification() {
 
   //put the notification bar in the feedback area which is sitting right after search bar
   var container = $('#exlidHeaderSystemFeedback');
-  container.prepend('<div id="alert-container"><div id="alert-icon-container"></div><div id="alert-text-container"><a aria-label="Log in" href="' + signInLink + '">Log in</a> to access full text, more search results and more services. <br>Refresh this page if you are logged in but still see this message.</div>');
+  container.prepend('<div id="alert-container"><div id="alert-icon-container"></div><div id="alert-text-container"><a aria-label="Log in" href="' + signInLink + '">Log in</a> to access full text, more search results and more services. Refresh this page if you are logged in but still see this message.</div>');
 }
 
 function modifyUserAreaTile() {
