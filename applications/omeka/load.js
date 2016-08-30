69c913dd8b8b79031afa542c144517adc0652964
Lea de Groot <l.degroot@library.uq.edu.au>
2016-08-30 12:01:53 +1000

    adding scripts to omeka [PT #128547681]

---
+++ applications/omeka/load.js
@@ -0,0 +1,85 @@
+function ready(fn) {
+console.log('running ready');
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+function loadReusableComponents() {
+  loadUQFavicon();
+
+  addAppleTouchIcon();
+
+  //insert elements, even before Polymer is loaded
+
+  //first element of the original document
+  var firstElement = document.body.children[0];
+
+  //insert alerts before header
+  var alerts = document.querySelector('uqlibrary-alerts');
+  if (!alerts) {
+    //as a back up insert header if it's not defined already
+    alerts = document.createElement('uqlibrary-alerts');
+    document.body.insertBefore(alerts, firstElement);
+  }
+
+  // insert header after body-tag
+  var header = document.createElement('uq-minimal-header');
+  document.body.insertBefore(header, firstElement);
+
+  // insert sub footer before body-tag
+  var subFooter = document.createElement('uql-connect-footer');
+  document.body.appendChild(subFooter);
+
+  // insert footer before body-tag
+  var footer = document.createElement('uq-minimal-footer');
+  document.body.appendChild(footer);
+
+
+  window.addEventListener('WebComponentsReady', function() {
+    // when polymer is ready - configure elements
+  });
+}
+
+function loadUQFavicon() {
+  var link = document.createElement('link'),
+    href = '//assets.library.uq.edu.au/master/reusable-components/resources/favicon.ico';
+  link.type = 'image/x-icon';
+  link.rel = 'shortcut icon';
+  link.href = href;
+  document.getElementsByTagName('head')[0].appendChild(link);
+  link.rel = 'icon'; //for IE
+  document.getElementsByTagName('head')[0].appendChild(link);
+
+}
+
+function addAppleTouchIcon() {
+  // replace apple-touch-icon
+  var appleTouchIconlink = $('link[rel="apple-touch-icon"]'),
+    link = document.createElement('link'),
+    sizes = ['152x152', '120x120', '76x76'],
+    rel = 'apple-touch-icon',
+    href = '//assets.library.uq.edu.au/master/reusable-components/resources/images/apple-touch-icon.png';
+
+  if (appleTouchIconlink) {
+    appleTouchIconlink.attr('href', href);
+  } else {
+    link.rel = rel;
+    link.href = href;
+    document.getElementsByTagName('head')[0].appendChild(link);
+  }
+
+  for (var i = 0; i < sizes.length; i++) {
+    var size = sizes[i],
+      iconLink = document.createElement('link');
+    iconLink.rel = rel;
+    iconLink.sizes = size;
+    iconLink.href = href.replace('icon.png','icon-' + size + '.png');
+    document.getElementsByTagName('head')[0].appendChild(iconLink);
+  }
+}
+
+
+ready(loadReusableComponents);
