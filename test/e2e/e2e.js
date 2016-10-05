b2231d343a58caec48dde198a035f6890c5550dc
Lea de Groot <l.degroot@library.uq.edu.au>
2016-10-05 10:36:11 +1000

    reorder tests [PT #129808567]

--- test/e2e/e2e.js
+++ test/e2e/e2e.js
@@ -1,25 +1,5 @@
 module.exports = {
 
-  'check https://www.studenthub.uq.edu.au/workgroups/library-staff-development is correctly calling reusable elements' : function (client) {
-    client
-      .url('https://www.studenthub.uq.edu.au/workgroups/library-staff-development')
-      .pause(20000) // allow saucelabs to get the page loaded
-      .waitForElementVisible('uql-askus-button', 10000)
-      .assert.elementPresent('uq-minimal-header', 'uq header component is present')
-      .assert.elementPresent('uq-minimal-header uql-global-links', 'uq global links header component is present')
-      .assert.containsText('.library-title a', 'UQ Library')
-      .assert.elementPresent('uql-askus-button', 'uql askus component is present')
-      .assert.elementPresent('uq-minimal-footer', 'uq footer component is present')
-      .assert.elementPresent('uq-minimal-footer uql-global-links', 'uq global links footer component is present')
-      .assert.containsText('uq-minimal-footer .footer-uq-details li', 'Authorised by:')
-      .assert.elementPresent('uq-minimal-footer .footer-legal-details a'
-      , 'Emergency Phone footer component is present')
-      .assert.containsText('uq-minimal-footer .footer-legal-details .h6', 'Emergency')
-      .assert.elementPresent('.sidebar .body > a', 'sidebar More Events button is present')
-      .assert.containsText('.sidebar .body > a', 'MORE EVENTS')
-      .end();
-  },
-
   'check uqlibraryonlineexhibitions.omeka.net is correctly calling reusable elements (if tests fail they may no longer be using omeka)' : function (client) {
     client
       .url('http://uqlibraryonlineexhibitions.omeka.net/exhibits/show/jd-fryer-student-and-soldier')
@@ -37,6 +17,25 @@ module.exports = {
                             , 'Emergency Phone footer component is present')
       .assert.containsText('uq-minimal-footer .footer-legal-details .h6', 'Emergency')
       .end();
-  }
+  },
 
+  'check https://www.studenthub.uq.edu.au/workgroups/library-staff-development is correctly calling reusable elements' : function (client) {
+    client
+      .url('https://www.studenthub.uq.edu.au/workgroups/library-staff-development')
+      .pause(20000) // allow saucelabs to get the page loaded
+      .waitForElementVisible('#sitename a', 10000)
+      .assert.elementPresent('uq-minimal-header', 'uq header component is present')
+      .assert.elementPresent('uq-minimal-header uql-global-links', 'uq global links header component is present')
+      .assert.containsText('.library-title a', 'UQ Library')
+      .assert.elementPresent('uql-askus-button', 'uql askus component is present')
+      .assert.elementPresent('uq-minimal-footer', 'uq footer component is present')
+      .assert.elementPresent('uq-minimal-footer uql-global-links', 'uq global links footer component is present')
+      .assert.containsText('uq-minimal-footer .footer-uq-details li', 'Authorised by:')
+      .assert.elementPresent('uq-minimal-footer .footer-legal-details a'
+      , 'Emergency Phone footer component is present')
+      .assert.containsText('uq-minimal-footer .footer-legal-details .h6', 'Emergency')
+      .assert.elementPresent('.sidebar .body > a', 'sidebar More Events button is present')
+      .assert.containsText('.sidebar .body > a', 'MORE EVENTS')
+      .end();
+  }
 };
