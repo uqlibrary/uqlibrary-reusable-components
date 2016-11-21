784fe12eaefd9acd1c8ea58fc2379b60f8518cbf
Lea de Groot <l.degroot@library.uq.edu.au>
2016-11-21 14:51:28 +1000

    move askus callout display contents from local json to uqlibrary-api [PT #134490463]

--- elements/uql-askus-button/uql-askus-button.js
+++ elements/uql-askus-button/uql-askus-button.js
@@ -10,49 +10,16 @@
       'uqlibrary-callout-summary-clicked': '_linkClicked'
     },
     attached: function () {
-      this.$.callout.calloutItems = {
-        "items": [
-          {
-            "label": "FAQ",
-            "icon": "communication:import-contacts",
-            "link": "http://answers.library.uq.edu.au/"
-          },
-          {
-            "label": "Chat",
-            "icon": "communication:chat",
-            "link": "https://v2.libanswers.com/chati.php?iid=1193&hash=fdbdf3c1190c1b6147b92d38c20194a8&online=true&referer=https%3A%2F%2Fwww.library.uq.edu.au%2F",
-            "disabled": "chat-offline",
-            "target": "chatWindow",
-            "targetOptions": "width=400, height=400"
-          },
-          {
-            "label": "Email",
-            "icon": "communication:email",
-            "link": "mailto:askus@library.uq.edu.au"
-          },
-          {
-            "label": "Phone",
-            "icon": "communication:call",
-            "link": "https://www.library.uq.edu.au/contact-us",
-            "linkMobile": "tel: +61733464312",
-            "disabled": "chat-offline"
-          },
-          {
-            "label": "Contact Form",
-            "icon": "device:dvr",
-            "link": "http://answers.library.uq.edu.au/ask"
-          },
-          {
-            "label": "Come see us",
-            "icon": "social:people",
-            "link": "https://www.library.uq.edu.au/locations-hours/service-points"
-          }
-        ],
-        "summary": {
-          "label": "More ways to contact us",
-          "link": "https://www.library.uq.edu.au/contact-us"
-        }
-      };
+      var self = this;
+
+      this.$.contactsApi.addEventListener('uqlibrary-api-contacts-loaded', function(e) {
+        self.$.callout.calloutItems = {
+          "items": e.detail.items,
+          "summary": e.detail.summary
+        };
+      });
+
+      this.$.contactsApi.get();
     },
 		/**
      * Called when a link is clicked in the uqlibrary-callout
