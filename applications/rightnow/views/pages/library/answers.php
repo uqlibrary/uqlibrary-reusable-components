e8759528da50524d1a4d994d9373eec53880f7aa
Lea de Groot <l.degroot@library.uq.edu.au>
2016-11-25 15:09:27 +1000

    add sidebar [PT #134490891]

--- applications/rightnow/views/pages/library/answers.php
+++ applications/rightnow/views/pages/library/answers.php
@@ -28,49 +28,85 @@
 </rn:container>
 
 <style type="text/css">
-  @font-face {
-    font-family: 'Material Icons';
-    font-style: normal;
-    font-weight: 400;
-    src: local('Material Icons'), local('MaterialIcons-Regular'), url(https://fonts.gstatic.com/s/materialicons/v18/2fcrYFNaTjcS6g4U3t-Y5UEw0lE80llgEseQY3FEmqw.woff2) format('woff2');
+  #rn_PageTitle,
+  #rn_PageContent,
+  #uq_AskQuestionContent {
+    background-color: #fff;
+    color: inherit;
+    border-radius: 2px;
+    border-width: 1px;
+    border-style: solid;
+    margin: 10px 0px 20px 0px;
+    border-color: #ccc;
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 1px 5px 0 rgba(0,0,0,.12),0 3px 1px -2px rgba(0,0,0,.2);
   }
-
-  .rn_Chat:before {
-    /*content: "communication:chat"*/
+  #rn_PageContent {
+    padding-top: 16px;
+  }
+  #rn_Body .rn_AnswerList {
+    width: 69%;
+  }
+  #rn_Body #rn_SearchControls {
+    padding-top: 16px;
+  }
+  .rn_AskQuestion {
+    padding-top: 1px;
+    padding-left: 70%;
+  }
+  #uq_AskQuestionContent h2 {
+    font-size: 16px;
+    margin: 0;
+    line-height: normal;
+    color: #333;
+    background-color: inherit;
+    padding: 16px;
+    border-bottom: 1px solid #ccc;
+  }
+  #uq_AskQuestionContent ul {
+    padding: 16px;
+  }
+  .rn_AskQuestion ul,
+  .rn_AskQuestion ul li {
+    list-style: none;
+    padding: 0 0 16px 0;
+    margin : 0;
+  }
+  .rn_AskQuestion ul li > span {
+    padding-right: 7px;
+  }
+  .rn_Hours,
+  .rn_HoursPrefix {
+    font-size: 0.9rem;
+  }
+  ul .rn_ChatHours .rn_HoursBlock {
+    margin-top: 0;
   }
 </style>
-<div id="uq_AskQuestionContent" class="rn_AskQuestion" style="padding-top: 40px; padding-left: 60%;">
-  <h2>AskUs contacts </h2>
-  <!--
-  http://documentation.custhelp.com/euf/assets/docs/november2016/olh/index_frames.html?q=/euf/assets/docs/november2016/olh/CustomerPortal/topicrefs/t_Add_a_chat_link_to_the_Support_Home_page_ax1229140.html
-  when unavailable:
-  <div id="rn_ConditionalChatLink_13" class="rn_ConditionalChatLink">
-  <div class="rn_Chat">
-  <a href="javascript:void(0);">Live Chat</a>
-  <span id="rn_ConditionalChatLink_13_UnavailableBusyMessage">Chat is not currently available.</span>
+<div class="rn_AskQuestion">
+  <div id="uq_AskQuestionContent">
+    <h2>AskUs contacts </h2>
+    <ul>
+      <li><rn:widget path="chat/ConditionalChatLink" open_in_new_window="true" chat_login_page_height="500" chat_login_page_width="500" />
+      </li>
+      <li>
+        <span><a aria-hidden="true" href="tel:61733464312"><iron-icon icon="communication:call"></iron-icon></a></span>
+        <a href="tel:61733464312">+ 61 7 334 64312</a>
+      </li>
+      <li>
+        <span><a aria-hidden="true" href="/app/library/contact"><iron-icon icon="communication:import-contacts"></iron-icon></a></span>
+        <a href="/app/library/contact">Contact form</a>
+      </li>
+      <li>
+        <span><a aria-hidden="true" href="mailto:askus@library.uq.edu.au"><iron-icon icon="communication:email"></iron-icon></a></span>
+        <a href="mailto:askus@library.uq.edu.au">askus@library.uq.edu.au</a>
+      </li>
+      <li>
+        <a href="http://www.library.uq.edu.au/contact-us">More ways to contact us</a>
+      </li>
+      <li>
+        <rn:widget path="chat/ChatHours"/>
+      </li>
+    </ul>
   </div>
-  </div>
-   -->
-  <rn:widget path="chat/ConditionalChatLink" open_in_new_window="true" chat_login_page_height="500" chat_login_page_width="500" />
-  <div>
-    <div>
-      <a aria-hidden="true" href="tel:61733464312"><iron-icon icon="communication:call"></iron-icon></a>
-      <a href="tel:61733464312">+ 61 7 334 64312</a>
-    </div>
-    <div>
-      <a aria-hidden="true" href="/ask"><iron-icon icon="communication:import-contacts"></iron-icon></a>
-      <a href="/ask">Contact form</a>
-    </div>
-    <a aria-hidden="true" href="mailto:askus@library.uq.edu.au"><iron-icon icon="communication:email"></iron-icon></a>
-    <a href="mailto:askus@library.uq.edu.au">askus@library.uq.edu.au</a>
-  </div>
-  <div>
-    <a href="http://www.library.uq.edu.au/contact-us">More ways to contact us</a>
-  </div>
-  <div>
-    <rn:widget path="chat/ChatHours"/>
-  </div>
-</div>
-
 
 
