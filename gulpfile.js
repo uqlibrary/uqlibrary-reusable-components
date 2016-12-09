20a92dca9fb677c553daaab0741357877f4f3c46
Lea de Groot <l.degroot@library.uq.edu.au>
2016-12-09 11:24:00 +1000

    remove unused code

--- gulpfile.js
+++ gulpfile.js
@@ -156,29 +156,6 @@ gulp.task('test:vulcanize', ['tests_call_vulcanized_elements'], function() {
 });
 
 
-gulp.task('tests_call_vulcanized_elements', function() {
-
-  var testFileRegEx = new RegExp('<link rel="import" href="../elements/(.*)/(.*).html">', "g");
-  var vulcanizedLinkElement = '<link rel="import" href="../elements.vulcanized.html.html">';
-
-  gulp.src(['test/uql-apps-button.html'
-            , 'test/uql-askus-button.html'
-            , 'test/uql-connect-footer-test.html'
-            , 'test/uql-drawer-panel-test.html'
-            , 'test/uql-ezproxy-test.html'
-            , 'test/uql-global-links-test.html'
-            , 'test/uql-ia-button-test.html'
-            , 'test/uql-login-button-test.html'
-            , 'test/uql-mega-menu-test.html'
-            , 'test/uql-menu-test.html'
-            , 'test/uql-search-button-test.html'
-          ])
-  .pipe(replace(testFileRegEx, vulcanizedLinkElement));
-
-
-
-});
-
 // display a list of available tasks
 gulp.task('help', taskList);
 
