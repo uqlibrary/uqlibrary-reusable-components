461d64531c9e219eb27d728260acbd0bfd2d8c7d
Lea de Groot <l.degroot@library.uq.edu.au>
2016-12-09 15:28:46 +1000

    update gulp copy

--- tasks/deployment-tasks.js
+++ tasks/deployment-tasks.js
@@ -119,7 +119,7 @@ gulp.task('copy:aws', function () {
       .pipe(gulp.dest(config.applications + '/bower_components/uqlibrary-api/mock'));
 
   var jsonData = gulp.src([config.dependencies + '/uqlibrary-api/data/contacts.json'])
-      .pipe(gulp.dest(config.applications + 'bower_components/uqlibrary-api/data'));
+      .pipe(gulp.dest(config.applications + 'bower_components/uqlibrary-api/data/*'));
 
   return merge(vulcanized, dependencies, resources, demo, mock_data, vulcanized2elements, jsonData)
       .pipe($.size({title: 'copy'}));
