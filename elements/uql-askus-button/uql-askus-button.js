e7666e9f7595d41342c90de2d8cc75ecad6b6e97
Lea de Groot <l.degroot@library.uq.edu.au>
2016-12-12 10:44:58 +1000

    revert data call [PT #134490297]

--- elements/uql-askus-button/uql-askus-button.js
+++ elements/uql-askus-button/uql-askus-button.js
@@ -10,16 +10,50 @@
       'uqlibrary-callout-summary-clicked': '_linkClicked'
     },
     attached: function () {
-      var self = this;
+      this.$.callout.calloutItems = {
+        "items": [
+        {
+          "label": "FAQ",
+          "icon": "communication:import-contacts",
+          "link": "https://uqcurrent.custhelp.com/app/library/faqs"
+        },
+        {
+          "label": "Chat",
+          "icon": "communication:chat",
+          "link": "https://uqcurrent.custhelp.com/app/chat/chat_launch_lib",
+          "disabled": "chat-offline",
+          "target": "chatWindow",
+          "targetOptions": "width=400, height=460"
+        },
+        {
+          "label": "Email",
+          "icon": "communication:email",
+          "link": "mailto:askus@library.uq.edu.au"
+        },
+        {
+          "label": "Phone",
+          "icon": "communication:call",
+          "link": "https://www.library.uq.edu.au/contact-us",
+          "linkMobile": "tel: +61733464312",
+          "disabled": "chat-offline"
+        },
+        {
+          "label": "Contact form",
+          "icon": "device:dvr",
+          "link": "https://uqcurrent.custhelp.com/app/library/contact"
+        },
+        {
+          "label": "Come see us",
+          "icon": "social:people",
+          "link": "https://web.library.uq.edu.au/locations-hours/service-points"
+        }
+      ],
+        "summary": {
+        "label": "More ways to contact us",
+          "link": "https://web.library.uq.edu.au/contact-us"
+      }
+      };
 
-      this.$.contactsApi.addEventListener('uqlibrary-api-contacts-loaded', function(e) {
-        self.$.callout.calloutItems = {
-          "items": e.detail.items,
-          "summary": e.detail.summary
-        };
-      });
-
-      this.$.contactsApi.get();
     },
 		/**
      * Called when a link is clicked in the uqlibrary-callout
