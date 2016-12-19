f697928d7ce0e6e093ac092bf6583622a2ab523d
Ky <k.lane@uq.edu.au>
2016-12-19 11:48:02 +1000

    [PT#136227451] New URL for uql-search-button

--- elements/uql-search-button/uql-search-button.js
+++ elements/uql-search-button/uql-search-button.js
@@ -18,7 +18,7 @@
       /** Value of redirect URL */
       redirectUrl: {
         type: String,
-        value: "http://www.uq.edu.au/search/?q=%20&as_sitesearch=library.uq.edu.au"
+        value: "http://www.uq.edu.au/search/?q=%20&as_sitesearch=library.uq.edu.au&cx=002364636408787706430:l-epmqjtgfs"
       }
     },
 
