ff52d3c3891396512795570b51b3379cf8912548
jannakha <j_malikova@yahoo.com>
2017-01-25 13:49:10 +1000

    fixing zindex on header

--- applications/primo2/load.js
+++ applications/primo2/load.js
@@ -10,18 +10,14 @@ function loadReusableComponents() {
   //insert elements, even before Polymer is loaded
 
   //first element of the original document
-  // var firstElement = document.body.children[0];
   var firstElement = document.querySelector('primo-explore div');
 
   //as a back up insert header if it's not defined already
   var alerts = document.createElement('uqlibrary-alerts');
-  // document.body.insertBefore(alerts, firstElement.childNodes[0]);
   firstElement.insertBefore(alerts, firstElement.firstChild);
 
   // insert header after body-tag
   var header = document.createElement('uq-minimal-header');
-  // document.body.insertBefore(header, firstElement.childNodes[0]);
-  // firstElement.appendChild(header);
   firstElement.insertBefore(header, firstElement.firstChild);
 
   // while new PrimoUI has infinite scrolling, do not include footer
@@ -33,10 +29,6 @@ function loadReusableComponents() {
   // // insert footer before body-tag
   // var footer = document.createElement('uq-minimal-footer');
   // document.body.appendChild(footer);
-
-  var app = angular.module('viewCustom', ['angularLoad']);
-  //
-  // app.component('prmSearchBarAfter', { template: "<p>HEllo</p>" });
 
   window.addEventListener('WebComponentsReady', function() {
     // when polymer is ready - configure elements
