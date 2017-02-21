2effbdab2ba7d15c7a912051189cd04e056baed7
jannakha <j_malikova@yahoo.com>
2017-02-21 17:31:30 +1000

    adding GTM to primo2 [#137683619]

--- applications/primo2/load.js
+++ applications/primo2/load.js
@@ -15,6 +15,7 @@ function loadReusableComponents() {
 
   // insert header inside primo's view
   var header = document.createElement('uq-minimal-header');
+  header.setAttribute("show-login-button", "false");
   firstElement.insertBefore(header, firstElement.firstChild);
 
   // insert alerts inside primo's view
@@ -34,7 +35,17 @@ function loadReusableComponents() {
 
   window.addEventListener('WebComponentsReady', function() {
     // when polymer is ready - configure elements
-    header.showLoginButton = false;
+    console.log('WebComponentsReady....');
+    console.dir(header);
+
+    // header.showLoginButton = false;
   });
 }
+
+//enable GTM
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W4KK37');
 
