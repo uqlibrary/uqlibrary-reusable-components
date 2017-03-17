322ecb28afbb80f390acfe516f93d2bbc8080f6a
Lea de Groot <l.degroot@library.uq.edu.au>
2017-03-17 14:00:05 +1000

    supply simpler chat button for Talis [PT #134563207]

--- applications/talis/load.js
+++ applications/talis/load.js
@@ -33,10 +33,47 @@ function loadReusableComponents() {
   document.body.appendChild(footer);
 
 
+
+
   window.addEventListener('WebComponentsReady', function() {
     // when polymer is ready - configure elements
     header.showLoginButton = false;
+    header.showChatButton = false;
   });
+
+  // this is temp code to add a simple contact button
+
+
+  var contactLink = 'https://web.library.uq.edu.au/contact-us';
+  var contactLinkIcon = 'https://raw.githubusercontent.com/google/material-design-icons/master/communication/2x_web/ic_forum_white_18dp.png';
+  var contactLinktext = 'Ask Us';
+
+  var chatIcon = document.createElement('img');
+  chatIcon.src = contactLinkIcon;
+
+  var chatIconBlock = document.createElement('div');
+  chatIconBlock.appendChild(chatIcon);
+
+  var textLabel = document.createTextNode(contactLinktext);
+
+  var textLabelBlock = document.createElement('span');
+  textLabelBlock.appendChild(textLabel);
+  textLabelBlock.style="";
+
+  var chatAnchor = document.createElement('a');
+  chatAnchor.href = contactLink;
+  chatAnchor.appendChild(chatIconBlock);
+  chatAnchor.appendChild(textLabelBlock);
+
+  var chatButton = document.createElement('div');
+  chatButton.className='TalisChatButton';
+  chatButton.appendChild(chatAnchor);
+
+  var headerBlock = document.querySelector('.actions.style-scope.uq-minimal-header');
+
+  headerBlock.appendChild(chatButton);
+
+
 }
 
 ready(loadReusableComponents);
