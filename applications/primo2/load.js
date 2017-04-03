e7266eef0d45fec6cf995acdfceaf36f0d849228
jannakha <j_malikova@yahoo.com>
2017-04-03 15:12:57 +1000

    debugging primo 2 issues

--- applications/primo2/load.js
+++ applications/primo2/load.js
@@ -1,89 +1,63 @@
+(function () {
+    "use strict";
 
-var primoViewHasLoaded = setInterval(function() {
-  //wait for primo's angular to load itself (eg replaces primo-explore contents with angular contents, then insert header)
-  if (document.querySelector('noscript') === null || typeof(document.querySelector('noscript')) === 'undefined' ) {
-    clearInterval(primoViewHasLoaded);
-    loadReusableComponents();
-  }
-}, 50);
-
-var app = angular.module('viewCustom', ['angularLoad']);
-createUserAreaCustomComponents(app);
-
-function insertUserArea(firstElement) {
-    var app = angular.element(firstElement);
-    var appInjector = app.injector();
-    var templateCache = appInjector.get('$templateCache');
-    templateCache.put('components/search/topbar/userArea/user-area.html',
-        '<div layout="row" layout-align="center center">' +
-        '<span class="hide-xs">{{$ctrl.userName()}}</span>' +
-        '<prm-search-bookmark-filter></prm-search-bookmark-filter>' +
-        '<prm-library-card-menu ng-show="$ctrl.userName().length > 0"></prm-library-card-menu>' +
-        '<prm-authentication layout="flex" [is-logged-in]="$ctrl.userName().length > 0"></prm-authentication>' +
-        '</div>'
-    );
-
-    var appRootScope = appInjector.get('$rootScope');
-    appInjector.invoke(function ($compile) {
-        $compile(app)(appRootScope);
-        appRootScope.$apply();
-    });
-}
-
-function createUserAreaCustomComponents(app) {
+    var app = angular.module('viewCustom', ['angularLoad']);
     app.component('prmUserAreaAfter', {
         bindings: {
             parentCtrl: '<'
         },
         controller: function($compile, $scope, $templateCache, $element) {
-          $templateCache.put('components/search/topbar/userArea/user-area.html',
-          '<div layout="row" layout-align="center center">' +
-          '<span class="hide-xs">{{$ctrl.userName()}}</span>' +
-          '<prm-search-bookmark-filter></prm-search-bookmark-filter>' +
-          '<prm-library-card-menu ng-show="$ctrl.userName().length > 0"></prm-library-card-menu>' +
-          '<prm-authentication layout="flex" [is-logged-in]="$ctrl.userName().length > 0"></prm-authentication>' +
-          '</div>'
-          );
+            $templateCache.put('components/search/topbar/userArea/user-area.html',
+                '<div layout="row" layout-align="center center">' +
+                '<span class="hide-xs">{{$ctrl.userName()}}</span>' +
+                '<prm-search-bookmark-filter></prm-search-bookmark-filter>' +
+                '<prm-library-card-menu ng-show="$ctrl.userName().length > 0"></prm-library-card-menu>' +
+                '<prm-authentication layout="flex" [is-logged-in]="$ctrl.userName().length > 0"></prm-authentication>' +
+                '</div>'
+            );
 
-          $compile($element.parent())($scope);
+            $compile($element.parent())($scope);
         }
     });
-}
+
+})();
+
+var primoViewHasLoaded = setInterval(function() {
+    //wait for primo's angular to load itself (eg replaces primo-explore contents with angular contents, then insert header)
+    if (document.querySelector('noscript') === null || typeof(document.querySelector('noscript')) === 'undefined' ) {
+        clearInterval(primoViewHasLoaded);
+        loadReusableComponents();
+    }
+}, 50);
 
 function loadReusableComponents() {
-  // insert elements, even before Polymer is loaded
-  // first element of the original document
-  var firstElement = document.querySelector('primo-explore');
+    // insert elements, even before Polymer is loaded
+    // first element of the original document
+    var firstElement = document.querySelector('primo-explore');
 
-  // insert updated user-area view for login/logout/account access, e2e tests this is applied
-  // insertUserArea(firstElement);
+    // insert header inside primo's view
+    var header = document.createElement('uq-minimal-header');
+    header.setAttribute("show-login-button", "false");
+    firstElement.insertBefore(header, firstElement.firstChild);
 
-  // insert header inside primo's view
-  var header = document.createElement('uq-minimal-header');
-  header.setAttribute("show-login-button", "false");
-  firstElement.insertBefore(header, firstElement.firstChild);
+    // insert alerts inside primo's view
+    var alerts = document.createElement('uqlibrary-alerts');
+    firstElement.insertBefore(alerts, firstElement.firstChild);
 
-  // insert alerts inside primo's view
-  var alerts = document.createElement('uqlibrary-alerts');
-  firstElement.insertBefore(alerts, firstElement.firstChild);
+    // while new PrimoUI has infinite scrolling, do not include footer
 
+    // // insert sub footer before body-tag
+    // var subFooter = document.createElement('uql-connect-footer');
+    // document.body.appendChild(subFooter);
+    //
+    // // insert footer before body-tag
+    // var footer = document.createElement('uq-minimal-footer');
+    // document.body.appendChild(footer);
 
-  // while new PrimoUI has infinite scrolling, do not include footer
-
-  // // insert sub footer before body-tag
-  // var subFooter = document.createElement('uql-connect-footer');
-  // document.body.appendChild(subFooter);
-  //
-  // // insert footer before body-tag
-  // var footer = document.createElement('uq-minimal-footer');
-  // document.body.appendChild(footer);
-
-  window.addEventListener('WebComponentsReady', function() {
-    // when polymer is ready - configure elements
-    // header.showLoginButton = false;
-  });
-
-
+    window.addEventListener('WebComponentsReady', function() {
+        // when polymer is ready - configure elements
+        // header.showLoginButton = false;
+    });
 }
 
 //enable GTM
