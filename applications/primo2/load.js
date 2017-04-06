d20f8aca6e23f480195bd7a34b79400b80eacfd0
jannakha <j_malikova@yahoo.com>
2017-04-06 17:38:53 +1000

    fixing animation [#141568463]

--- applications/primo2/load.js
+++ applications/primo2/load.js
@@ -20,13 +20,6 @@ function insertReusableComponents() {
     var alerts = document.createElement('uqlibrary-alerts');
     firstElement.insertBefore(alerts, firstElement.firstChild);
 
-    // move user area into header, by this time Polymer and Angular should be loaded
-    var header = document.querySelector('uq-minimal-header');
-    var userArea = document.querySelector('prm-user-area');
-    if (header && userArea) {
-        header.appendChild(userArea);
-    }
-
     // while new PrimoUI has infinite scrolling, do not include footer
     // // insert sub footer before body-tag
     // var subFooter = document.createElement('uql-connect-footer');
@@ -35,8 +28,21 @@ function insertReusableComponents() {
     // // insert footer before body-tag
     // var footer = document.createElement('uq-minimal-footer');
     // document.body.appendChild(footer);
-}
 
+    var headerHasLoaded = setInterval(function() {
+        //wait for primo's angular to load itself (eg replaces primo-explore contents with angular contents, then insert header)
+        if (document.querySelector('uqlibrary-ga') !== null || typeof(document.querySelector('uqlibrary-ga')) !== 'undefined' ) {
+            clearInterval(headerHasLoaded);
+
+            // move user area into header when polymer has loaded
+            var header = document.querySelector('uq-minimal-header');
+            var userArea = document.querySelector('prm-user-area');
+            if (header && userArea) {
+                header.appendChild(userArea);
+            }
+        }
+    }, 50);
+}
 
 // enable GTM
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
