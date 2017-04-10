e44548e797f32a6d56956cf83ca68b756fd8d19b
jannakha <j_malikova@yahoo.com>
2017-04-10 15:21:14 +1000

    fixing animation [#141568463]

--- applications/primo2/load.js
+++ applications/primo2/load.js
@@ -1,3 +1,18 @@
+
+
+var fixUserArea = setInterval(function() {
+    //wait for header to be inserted by angular
+    if (document.querySelector('uq-minimal-header') !== null && typeof(document.querySelector('uq-minimal-header')) !== 'undefined') {
+        clearInterval(fixUserArea);
+
+        //remove unused favourites element
+        var favouritesElement = document.querySelector('.top-nav-bar > prm-search-bookmark-filter');
+        if (favouritesElement !== null && typeof(favouritesElement) !== 'undefined') {
+            favouritesElement.parentNode.removeChild(favouritesElement);
+        }
+    }
+}, 100);
+
 // enable GTM
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
