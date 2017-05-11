1ae51911d462f21e05f88b55a5a0544d1958668e
Ky <k.lane@uq.edu.au>
2017-05-11 13:09:14 +1000

    Added element waits in e2e primo2 test, as the tests were timing out before rendering as expected

--- test/e2e/e2e.omeka.js
+++ test/e2e/e2e.omeka.js
@@ -2,19 +2,19 @@ var minimalUql = require("./e2e.minimal.js");
 var urlTest = 'http://uqlibraryonlineexhibitions.omeka.net/exhibits/show/jd-fryer-student-and-soldier';
 
 module.exports = {
-  '@tags': ['e2etest', 'omeka'],
+    '@tags': ['e2etest', 'omeka'],
 
-  'Test reusable components are applied to Omeka' : function (client) {
-    // common uql checks
-    minimalUql.commonChecks(client, urlTest);
+    'Test reusable components are applied to Omeka': function (client) {
+        // common uql checks
+        minimalUql.commonChecks(client, urlTest);
 
-    // omeka specific checks
-    client
-      .assert.containsText('.application-title', 'Online Exhibitions')
-      .end();
+        // omeka specific checks
+        client
+            .pause(5000)
+            .assert.containsText('.application-title', 'Online Exhibitions')
+            .end();
 
-  }
-
+    }
 
 
 };
