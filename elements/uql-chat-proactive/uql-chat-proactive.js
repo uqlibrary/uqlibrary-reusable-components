c365c3fd023a6608fd1fad4b0f58cbd7ace819d2
Lea de Groot <l.degroot@library.uq.edu.au>
2018-01-03 15:14:50 +1000

    needs to check from element available on root and result page [PT #153837535]

--- elements/uql-chat-proactive/uql-chat-proactive.js
+++ elements/uql-chat-proactive/uql-chat-proactive.js
@@ -146,7 +146,7 @@
      * @private
      */
     _watchForPrimoFiltersButton: function() {
-      var facetsSidebar = document.querySelector('#facets');
+      var facetsSidebar = document.querySelector('prm-explore-main');
       var filterDivs = document.getElementsByClassName('multiselect-submit-inner');
       var foundDiv = document.body.contains(filterDivs);
       var observer = new MutationObserver(function(mutations) {
