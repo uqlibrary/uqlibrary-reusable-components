612e8c390f23960059145427bccedffcbdac4cec
Lea de Groot <l.degroot@library.uq.edu.au>
2018-01-05 10:37:22 +1000

    cleanup [PT #153837535]

--- elements/uql-chat-proactive/uql-chat-proactive.js
+++ elements/uql-chat-proactive/uql-chat-proactive.js
@@ -39,7 +39,7 @@
       numberMillsecondsBeforePopup: {
         type: Number,
         value: 60000
-        // if you change this, consider the time in _setPopupMaxWidthInPrimo
+        // if you change this value, consider the time in _setPopupMaxWidthInPrimo
       },
 
       cookieNameNoPopup: {
@@ -141,31 +141,27 @@
      * On the primo results page, change the width of the proactive chat popup
      * so that it doesnt overlap the results list
      * because there are yucky things happening there with z-index.
-     * If they load the page with no query, thewre is no sidebar - async gets around that
+     * If they load the page with no query, there is no sidebar yet - async gets around that
      * @private
      */
     _setPopupMaxWidthInPrimo: function() {
-console.log('start _setPopupMaxWidthInPrimo');
       var numMilliSecondsRecheck = 10000; // 10 seconds - we have 60 seconds before the popup loads. give them time to type in their query...
+
       var sidebarWidthString;
       var proactivechat;
       var facets;
 
       this.async(function () {
-console.log('in _setPopupMaxWidthInPrimo async');
         facets = document.querySelector('#facets');
         if (facets) {
           sidebarWidthString = window.getComputedStyle(facets, null).getPropertyValue('width').trim();
-console.log('sidebarWidthString = ' + sidebarWidthString);
         }
         if (sidebarWidthString) {
           proactivechat = document.querySelector('.proactivechat paper-card');
         }
         if (proactivechat) {
           proactivechat.style.maxWidth = sidebarWidthString;
-console.log('max width set');
         } else {
-console.log('not found - run again later');
           this._setPopupMaxWidthInPrimo();
         }
       }, numMilliSecondsRecheck);
@@ -209,6 +205,7 @@ console.log('not found - run again later');
 
     /*
      * the amount of space needed to allow the 'apply filters' button to appear
+     * pixels
      */
     _setPrimoFilterButtonPositioning: function(bottomMargin) {
       this.filterButtonDivMarginBottom = bottomMargin;
