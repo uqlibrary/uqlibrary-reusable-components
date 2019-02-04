5548413f962038015de205b33839c3aa1efcf404
Lea de Groot <l.degroot@library.uq.edu.au>
2019-02-04 10:24:21 +1000

    updating osx tests to Mojave [PT #163685055]

--- wct.conf.js
+++ wct.conf.js
@@ -11,10 +11,10 @@ module.exports = {
                 'Windows 10/chrome',
                 'Windows 10/firefox',
                 'Windows 10/firefox@60', // ESR - check # at https://www.mozilla.org/en-US/firefox/organizations/
-                'OS X 10.13/safari',
-                'OS X 10.13/firefox',
-                'OS X 10.13/firefox@60',
-                'OS X 10.13/chrome'
+                'OS X 10.14/safari',
+                'OS X 10.14/firefox',
+                'OS X 10.14/firefox@60',
+                'OS X 10.14/chrome'
             ]
         },
         local: {
