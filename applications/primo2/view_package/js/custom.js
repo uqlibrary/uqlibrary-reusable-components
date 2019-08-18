cff5d303765e9cf5d173af44c7b5d2668214939a
Lea de Groot <l.degroot@library.uq.edu.au>
2019-08-19 08:13:21 +1000

    remove map it from sandbox prod [PT #167749020]

--- applications/primo2/view_package/js/custom.js
+++ applications/primo2/view_package/js/custom.js
@@ -233,29 +233,3 @@
   insertScript(scripts[2]);
 
 })();
-
-/* StackMap: Start */
-(function(){
-
-  var a = document.querySelector("head");
-  var css1 = document.createElement("link");
-  css1.type = "text/css";
-  css1.rel = "Stylesheet";
-  css1.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
-  css1.crossorigin = "anonymous";
-  a.appendChild(css1);
-
-  var css2 = document.createElement("link");
-  css2.type = "text/css";
-  css2.rel = "Stylesheet";
-  css2.href = "https://www.stackmap.com/integration/uq-primo2/StackMap.css";
-  a.appendChild(css2);
-
-  var w = document.createElement("script");
-  w.type = "text/javascript"; w.async = true;
-  w.src = "https://www.stackmap.com/integration/uq-primo2/StackMap.js";
-  var b = document.body;
-  b.appendChild(w);
-
-})();
-/* StackMap: END */
