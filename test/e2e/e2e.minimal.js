5f77cd85394b8bbcba45a676ba22403398f3b3d8
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2020-04-21 16:07:11 +1000

    [#171562848] Replace wait method, remove redundancy
    - waitForElementVisible() doesn't work on Safari: https://github.com/nightwatchjs/nightwatch/issues/2185

--- test/e2e/e2e.minimal.js
+++ test/e2e/e2e.minimal.js
@@ -1,19 +1,24 @@
 // check for elements common across the uql looknfeel
 module.exports = {
-  commonChecks: function (browser, urlTest) {
+  commonChecks: function (browser, urlTest, testFooter = true) {
     browser
       .url(urlTest)
       .pause(10000) // allow time for the page to load minimal components
+      .waitForElementPresent('uq-minimal-header', 20000)
       .assert.elementPresent('uq-minimal-header', 'uq header component is present')
       .assert.elementPresent('uq-minimal-header uql-global-links', 'uq global links header component is present')
       .assert.containsText('.library-title a', 'Library')
-      .assert.elementPresent('uql-askus-button', 'uql askus component is present')
-      .assert.elementPresent('uq-minimal-footer', 'uq footer component is present')
-      .assert.elementPresent(
-        'uq-minimal-footer .footer-legal-details a',
-        'Emergency Phone footer component is present'
-      )
-      .assert.containsText('uq-minimal-footer .footer-legal-details .h6', 'Emergency');
+      .assert.elementPresent('uql-askus-button', 'uql askus component is present');
+
+    if (testFooter) {
+      browser
+        .assert.elementPresent('uq-minimal-footer', 'uq footer component is present')
+        .assert.elementPresent(
+          'uq-minimal-footer .footer-legal-details a',
+          'Emergency Phone footer component is present'
+        )
+        .assert.containsText('uq-minimal-footer .footer-legal-details .h6', 'Emergency');
+    }
     return browser;
   }
 };
