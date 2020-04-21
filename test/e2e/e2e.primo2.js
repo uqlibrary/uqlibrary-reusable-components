0d26ec43a7cfa8402bd9180301ae3c47e7d2b86b
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2020-04-21 16:07:11 +1000

    [#171562848] Replace wait method, remove redundancy
    - waitForElementVisible() doesn't work on Safari: https://github.com/nightwatchjs/nightwatch/issues/2185

--- test/e2e/e2e.primo2.js
+++ test/e2e/e2e.primo2.js
@@ -1,3 +1,5 @@
+var minimalUql = require("./e2e.minimal.js");
+
 // test for something basic that should always return results
 var searchText = 'book';
 var urlTest = 'https://search.library.uq.edu.au/primo-explore/search?query=any,contains,' + searchText + '&tab=61uq_all&search_scope=61UQ_All&sortby=rank&vid=61UQ&offset=0';
@@ -7,15 +9,10 @@ module.exports = {
 
   'Test reusable components are applied to New Primo UI': function (client) {
     // common uql checks
+    // new primo UI doesn't have footer, only header
+    minimalUql.commonChecks(client, urlTest, false);
+
     client
-      .url(urlTest)
-      .pause(5000) // allow time for the page to load minimal components
-      // new primo UI doesn't have footer, only header
-      .waitForElementVisible('uq-minimal-header', 20000)
-      .assert.elementPresent('uq-minimal-header', 'uq header component is present')
-      .assert.elementPresent('uq-minimal-header uql-global-links', 'uq global links header component is present')
-      .assert.containsText('.library-title a', 'Library')
-      .assert.elementPresent('uql-askus-button', 'uql askus component is present')
       // primo specific checks
       .assert.elementPresent('prm-brief-result-container', 'at least one primo result is present')
       .end();
@@ -24,8 +21,8 @@ module.exports = {
   'Test user area modifications have been applied': function (client) {
     client
       .url(urlTest)
-      .pause(5000)
-      .waitForElementVisible('prm-topbar', 20000)
+      .pause(10000)
+      .waitForElementPresent('prm-topbar', 20000)
       .assert.elementPresent('prm-topbar prm-user-area', 'user area component is present')
       .assert.elementPresent('prm-topbar prm-user-area prm-authentication', 'login component is present')
       .assert.elementPresent('prm-topbar prm-user-area prm-authentication span', 'login text is present')
